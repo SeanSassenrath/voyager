4059f22b35d884f39690269d83ca01558a646014
SeanSassenrath <sean.sassenrath@gmail.com>
2015-10-09 17:55:23 -0700

    changing nav style

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,10 +1,8 @@
-var map;
 
 (function initialize() {
-  alert('hello');
   var point = new google.maps.LatLng(-33.8665433, 151.1956316);
 
-  map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('map'), {
     center: point,
     zoom: 15
   });
