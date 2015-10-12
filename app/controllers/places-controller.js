732ccf9157018a9830acfca98cabe51290130d05
SeanSassenrath <sean.sassenrath@gmail.com>
2015-10-12 16:49:43 -0700

    adding img files

--- app/controllers/places-controller.js
+++ app/controllers/places-controller.js
@@ -1,18 +1,25 @@
 'use strict';
 
 angular.module('placesCtrl', [])
-    .controller('PlacesCtrl', function(Places){
+    .controller('PlacesCtrl', function($scope){
 
     var ctrl = this;
 
     ctrl.newPlace = {
       activity: "",
-      location: "current location",
+      location: "",
     };
+
+    ctrl.places = ['test', 'test2']
+
+    ctrl.placesTest = function() {
+      console.log("Ctrl Places being fired", ctrl.places)
+      return ctrl.places;
+    }
 
     ctrl.submit = function(place) {
       console.log("New place = ", place.activity + " " + place.location);
-      if(place.location === "current location") {
+      if(place.location === "" || place.location === "current location") {
         ctrl.getCurrentLocation(place)
       } else {
         ctrl.getLocation(place)
@@ -26,11 +33,10 @@ angular.module('placesCtrl', [])
 
       var map = new google.maps.Map(document.getElementById('map'), {
         center: point,
-        zoom: 15
+        zoom: 14
       })
 
       var infowindow = new google.maps.InfoWindow();
-
 
       var request = {
         location: point,
@@ -43,9 +49,13 @@ angular.module('placesCtrl', [])
 
       function callback(results, status) {
         if (status === "OK") {
+          // console.log("Results Array ", results)
+          ctrl.places = results;
+          console.log('Ctrl places ', ctrl.places)
           for(var i = 0; i < results.length; i++) {
             createMarker(results[i])
-            console.log(results[i])
+            $scope.$apply();
+            // console.log(results[i])
           }
         } else {
           console.log("Status ", status)
@@ -73,7 +83,6 @@ angular.module('placesCtrl', [])
           var lng = position.coords.longitude;
           console.log("About to pass in coords to map ", lat, lng)
           ctrl.showMap(lat, lng, place);
-          // ctrl.nearbyPlaces()
         })
       }
 
