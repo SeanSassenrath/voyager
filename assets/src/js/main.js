f04dcbcaf41ef49ad2072bd24e752d439b0ba158
SeanSassenrath <sean.sassenrath@gmail.com>
2015-10-16 10:39:41 -0700

    removing console.logs

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,14 +1,5 @@
+//Future jQuery snippets go here
+
 $(document).ready(function() {
   console.log("jQuery is present")
 })
-// (function initialize() {
-
-//   console.log('JS is here')
-//   // var point = new google.maps.LatLng(-33.8665433, 151.1956316);
-
-//   // var map = new google.maps.Map(document.getElementById('map'), {
-//   //   center: point,
-//   //   zoom: 15
-//   // });
-
-// })();
